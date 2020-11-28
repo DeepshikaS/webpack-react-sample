5cb61031b938cae75bc696f5f397d79d470ce05c
DeepshikaS <ddeepu13@gmail.com>
2020-11-28 22:35:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React, {  Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
-class App extends Component{
-  state = {
-    loading: true,
-    person: null
-  }
-  async componentDidMount() {
+const App = () => {
+  const [PersonName, setPersonName] = useState("");
+  const [PersonCell, setPersonCell] = useState("");
+  const [PersonImage, setPersonImage] = useState("");
+  const [PersonEmail, setPersonEmail] = useState("");
 
-    const url = 'https://randomuser.me/api/';
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
+  const PersonData = async () => {
+    try {
+      const res = await axios.get("https://randomuser.me/api/");
+      setPersonCell(res.data.results[0].cell);
+      setPersonEmail(res.data.results[0].email);
+      setPersonImage(res.data.results[0].picture.large);
+      setPersonName(
+        `${res.data.results[0].name.first} ${res.data.results[0].name.last}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  }
+  useEffect(() => {
+    PersonData();
+  }, []);
 
-  render() {
-
-    return (
-    <div className="App">
-      { this.state.loading || !this.state.person ? (
-          <div>loading...</div>
-        ) : (
-          <div className="person">
-            <p >Random Person from API</p>
-            <div >Title: { this.state.person.name.title }</div>
-            <div >First: { this.state.person.name.first }</div>
-            <div>Last: { this.state.person.name.last }</div>
-            <img src={ this.state.person.picture.large } />
-          </div>
-          )}
+  return (
+    <div className="person" style={{ alignContent:"center" }}>
+      <div className="card">
+        <img src={PersonImage} style={{ width: "30%" }} />
+        <h1>{PersonName}</h1>
+        <p className="title">{PersonEmail}</p>
+        <p>{PersonCell}</p>
+        <button onClick={() => PersonData()}>New Person</button>
+      </div>
     </div>
-    );
-  }
-}
-
+  );
+};
 
 export default App;
